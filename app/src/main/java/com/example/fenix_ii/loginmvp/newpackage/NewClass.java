c9533ac99a2a2cf5f6c42021c75bf896a1ca6da0
Nikhil <nyayadhish.nikhil20@gmail.com>
2016-08-18 13:44:04 +0530

    I addded a new class file

---
+++ app/src/main/java/com/example/fenix_ii/loginmvp/newpackage/NewClass.java
@@ -0,0 +1,24 @@
+package com.example.fenix_ii.loginmvp.newpackage;
+
+/**
+ * Created by fenix-ii on 18/8/16.
+ */
+public class NewClass {
+    /*
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    * Test Code
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    * */
+}
